ba9bb225a9e19f3f803ec1bac8fc5d701cfa173d
abdulkyume <abbirtak3@gmail.com>
2024-05-11 09:48:45 +0600

    css fix for faq

--- src/app/core/interceptor/jwt.interceptor.ts
+++ src/app/core/interceptor/jwt.interceptor.ts
@@ -2,30 +2,36 @@ import { inject } from '@angular/core';
 import { HttpRequest, HttpHandlerFn } from '@angular/common/http';
 import { timeout } from 'rxjs';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 export function JwtInterceptor(
   request: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) {
-  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (router.url !== '/public/faq') {
+    const authService = inject(AuthService);
 
-  let localdata = localStorage.getItem('expires');
-  let expires = new Date(localStorage.getItem('expires')!).getTime() - 10000;
-  let today = new Date().getTime();
+    let localdata = localStorage.getItem('expires');
+    let expires = new Date(localStorage.getItem('expires')!).getTime() - 10000;
+    let today = new Date().getTime();
 
-  if (localdata) {
-    if (expires <= today) {
-      authService.logout();
+    if (localdata) {
+      if (expires <= today) {
+        authService.logout();
+      }
     }
+
+    const currentUser = authService.currentUserValue;
+    if (currentUser && currentUser.refreshToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + currentUser.refreshToken,
+        },
+      });
+    }
+    return next(request).pipe(timeout(60000));
   }
 
-  const currentUser = authService.currentUserValue;
-  if (currentUser && currentUser.refreshToken) {
-    request = request.clone({
-      setHeaders: {
-        Authorization: 'Bearer ' + currentUser.refreshToken,
-      },
-    });
-  }
   return next(request).pipe(timeout(60000));
 }
