b0516141db84a3fd51c36389257fb1b6fd24f7c0
abdulkyume <abbirtak3@gmail.com>
2024-06-29 11:22:38 +0600

    inventory implementation

--- src/app/core/service/inventory-sales.service.ts
+++ src/app/core/service/inventory-sales.service.ts
@@ -135,4 +135,125 @@ export class InventorySalesService {
       })
     );
   }
+
+  getAllInventory(
+    page: number = 0,
+    limit: number = 10,
+    inventoryItemId: string = '',
+    shopId: string = '',
+    orderId: string = '',
+    dsoId: string = '',
+  ) {
+    var params = new HttpParams()
+      .set('page', Number(page))
+      .set('pageSize', Number(limit))
+      .set('inventoryItemId', String(inventoryItemId))
+      .set('shopId', String(shopId))
+      .set('dsoId', String(dsoId))
+      .set('orderId', String(orderId));
+    return this.http.get(`${this.apiurl}${this.dueEntry}/get`, { params }).pipe(
+      timeout(60000),
+      catchError((err) => {
+        console.error(err);
+        if (err.name === 'TimeoutError') {
+          Swal.fire('Time Out!!', 'Internal Server Problem');
+        }
+        if (err === 'Bad Request') {
+          Swal.fire('Error!!', 'Form Submission Error');
+        }
+        if (err === 'Unknown Error') {
+          Swal.fire('Error!!', 'No Connection Found');
+        }
+        throw err;
+      })
+    );
+  }
+
+  addInventory(newUser: any) {
+    console.log(newUser)
+    var apiurl = environment.environment;
+    return this.http.post<any>(`${apiurl}${this.dueEntry}/add`, newUser).pipe(
+      timeout(60000),
+      catchError((err) => {
+        console.error(err);
+        if (
+          err === 'Unauthorized user.' ||
+          err.message === 'Unauthorized user.'
+        ) {
+          this.authService.logout();
+        }
+        if (err.name === 'TimeoutError') {
+          Swal.fire('Time Out!!', 'Internal Server Problem');
+        }
+        if (err === 'Bad Request') {
+          Swal.fire('Error!!', 'Form Submission Error');
+        }
+        if (err === 'Unknown Error') {
+          Swal.fire('Error!!', 'No Connection Found');
+        }
+        throw err;
+      })
+    );
+  }
+
+  updateInventory(newUser: any) {
+    var apiurl = environment.environment;
+    return this.http.put<any>(`${apiurl}${this.dueEntry}/update`, newUser).pipe(
+      timeout(60000),
+      catchError((err) => {
+        console.error(err);
+        if (
+          err === 'Unauthorized user.' ||
+          err.message === 'Unauthorized user.'
+        ) {
+          this.authService.logout();
+        }
+        if (err.name === 'TimeoutError') {
+          Swal.fire('Time Out!!', 'Internal Server Problem');
+        }
+        if (err === 'Bad Request') {
+          Swal.fire('Error!!', 'Form Submission Error');
+        }
+        if (err === 'Unknown Error') {
+          Swal.fire('Error!!', 'No Connection Found');
+        }
+        if (
+          err.message === "Cannot read properties of null (reading 'message')"
+        ) {
+          Swal.fire(
+            'Error!!',
+            'Resource Not Available. Link is Not Working',
+            'error'
+          );
+        }
+        throw err;
+      })
+    );
+  }
+
+  deleteInventory(id: any) {
+    var apiurl = environment.environment;
+    return this.http.delete<any>(`${apiurl}${this.dueEntry}/delete/${id}`).pipe(
+      timeout(60000),
+      catchError((err) => {
+        console.error(err);
+        if (
+          err === 'Unauthorized user.' ||
+          err.message === 'Unauthorized user.'
+        ) {
+          this.authService.logout();
+        }
+        if (err.name === 'TimeoutError') {
+          Swal.fire('Time Out!!', 'Internal Server Problem');
+        }
+        if (err === 'Bad Request') {
+          Swal.fire('Error!!', 'Form Submission Error');
+        }
+        if (err === 'Unknown Error') {
+          Swal.fire('Error!!', 'No Connection Found');
+        }
+        throw err;
+      })
+    );
+  }
 }
--- src/app/pages/invesale/inventory/inventory.component.ts
+++ src/app/pages/invesale/inventory/inventory.component.ts
@@ -1,13 +1,281 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { InventorySalesService } from 'src/app/core/service/inventory-sales.service';
+import { RoleService } from 'src/app/core/service/role.service';
+import { EncryptionService } from 'src/app/core/service/encryption.service';
+import Swal from 'sweetalert2';
+import { LoaderComponent } from 'src/app/common/loader/loader.component';
+import { NgbDropdownModule, NgbPagination } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    LoaderComponent,
+    NgbPagination,
+    ReactiveFormsModule,
+    NgbDropdownModule,
+    NgMultiSelectDropDownModule,
+    FormsModule,
+  ],
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<any> = new Subject();
+  loader: boolean = true;
+  showAddBtn: boolean = true;
+  page: number = 0;
+  pageSize: number = 10;
+  cPageVal: number = 1;
+  toPageVal: number = 0;
+  total: number = 0;
+  userid: string = '';
+  invList: any[] = [];
+  invSForm!: FormGroup;
+  invForm!: FormGroup;
+  dropdownSettings = {};
+  dropdownList = [];
+  selectedItems: any;
+  dropdownList1 = [];
+  selectedItems1: any;
 
+  private insService = inject(InventorySalesService);
+  private roleService = inject(RoleService);
+  private encryptionService = inject(EncryptionService);
+  private formBuilder = inject(FormBuilder);
+
+  get f() {
+    return this.invForm.controls;
+  }
+
+  ngOnInit(): void {
+    this.dropdownSettings = {
+      singleSelection: true,
+      idField: 'id',
+      textField: 'value',
+      itemsShowLimit: 5,
+      allowSearchFilter: true,
+    };
+    this.getAllDSO();
+    this.userid = JSON.parse(
+      this.encryptionService.decrypt(localStorage.getItem('currentUser')!)
+    ).id;
+
+    this.dueSRefreshForm();
+    this.getAllDue();
+    this.dueRefreshForm();
+  }
+
+  getAllDSO(): void {
+    let data: any = [];
+    this.roleService
+      .getAllusers(0, 1000, '', 'e4151052-bf99-947b-1c8e-ee934cc685d5')
+      .subscribe({
+        next: (res: any) => {
+          res.data.content.map((content: any) => {
+            data.push({
+              id: content.id,
+              value: `${content.firstName} ${content.lastName} - ${content.mobile}`,
+            });
+          });
+        },
+        error: (error: any) => {
+          this.loader = false;
+          console.error('Error:', error);
+        },
+        complete: () => {
+          this.dropdownList = data;
+        },
+      });
+  }
+
+  dueSRefreshForm(): void {
+    this.invSForm = this.formBuilder.group({
+      name: [''],
+      vendor: [''],
+    });
+  }
+
+  dueRefreshForm(): void {
+    this.invForm = this.formBuilder.group({
+      id: [''],
+      name: [''],
+      vendor: [''],
+      deleted: [false],
+    });
+  }
+
+  toggleAddBtn(): void {
+    this.showAddBtn ? (this.showAddBtn = false) : (this.showAddBtn = true);
+  }
+
+  onsubmit() {
+    if (this.invForm.invalid) {
+      return;
+    }
+    this.loader = true;
+    if (this.invForm.controls['id'].value) {
+      this.insService
+        .updateDue(this.invForm.value)
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe({
+          next: (res: any) => {
+            if (!res.isSuccess) {
+              this.loader = false;
+              this.errorssmsg(res.reason);
+            } else {
+              this.successmsg(res.reason);
+              this.dueRefreshForm();
+              this.showAddBtn = true;
+              this.getAllDue();
+            }
+          },
+          error: (err: any) => {
+            console.error(err);
+            this.loader = false;
+          },
+          complete: () => {
+            this.loader = false;
+          },
+        });
+    } else {
+      this.insService
+        .addDue(this.invForm.value)
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe({
+          next: (res: any) => {
+            if (!res.isSuccess) {
+              this.loader = false;
+              this.errorssmsg(res.reason);
+            } else {
+              this.successmsg(res.reason);
+              this.dueRefreshForm();
+              this.showAddBtn = true;
+              this.getAllDue();
+            }
+          },
+          error: (err: any) => {
+            console.error(err);
+            this.loader = false;
+          },
+          complete: () => {
+            this.loader = false;
+          },
+        });
+    }
+  }
+
+  getAllDue(): void {
+    this.insService
+      .getAllDue(
+        this.page,
+        this.pageSize,
+        this.invSForm.controls['name'].value,
+        this.invSForm.controls['vendor'].value
+      )
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.invList = res.data.content;
+          this.total = res.data.totalElements!;
+          if (this.toPageVal > this.total) {
+            this.toPageVal = this.total;
+          } else {
+            this.toPageVal = this.invList.length;
+          }
+        },
+        error: (err: any) => {
+          console.error(err);
+          this.loader = false;
+        },
+        complete: () => {
+          this.loader = false;
+        },
+      });
+  }
+
+  editDue(data: any) {
+    this.selectedItems = this.dropdownList.filter(
+      (item: any) => item.id === data.id
+    );
+    this.selectedItems1 = this.dropdownList1.filter(
+      (item: any) => item.id === data.id
+    );
+    this.invForm.controls['id'].setValue(data.id);
+    this.invForm.controls['name'].setValue(data.name);
+    this.invForm.controls['vendor'].setValue(data.vendor);
+    this.showAddBtn = false;
+    window.scrollTo(0, 0);
+  }
+
+  deleteDue(id: string): void {
+    this.loader = true;
+    this.insService.deleteDue(id).subscribe({
+      next: (res: any) => {
+        if (!res) {
+          this.getAllDue();
+        }
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.loader = false;
+      },
+      complete: () => {
+        this.loader = false;
+      },
+    });
+  }
+
+  onPageChange(event: number): number {
+    this.loader = true;
+    this.toPageVal = event * this.pageSize;
+    this.page = event - 1;
+
+    this.cPageVal = (event - 1) * this.pageSize + 1;
+
+    if (this.toPageVal > this.total) {
+      this.toPageVal = this.total;
+    }
+    this.getAllDue();
+    return event;
+  }
+
+  onInitiatorItemSelect(item: any): void {
+    this.f['name'].setValue(item);
+  }
+
+  onInitiatorItemUnSelect(item: any): void {
+    this.f['name'].setValue('');
+  }
+
+  onInitiatorItemSelect1(item: any): void {
+    this.f['vendor'].setValue(item);
+  }
+
+  onInitiatorItemUnSelect1(item: any): void {
+    this.f['vendor'].setValue('');
+  }
+
+  successmsg(message: string): void {
+    Swal.fire('Success!', message, 'success');
+  }
+
+  errorssmsg(message: string): void {
+    Swal.fire('Ops!', message, 'error');
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next(undefined);
+    this.ngUnsubscribe.complete();
+  }
 }
